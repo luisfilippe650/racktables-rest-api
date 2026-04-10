9e65a168fbc2f1c5d8d9a6157ec9b3a5e93fa83f
Luis Filippe <seu_email_do_github>
2026-04-10 09:46:31 -0300

    add new version 1.0 and route in main.py

--- app/main.py
+++ app/main.py
@@ -4,13 +4,18 @@ from app.routers.rackspace.rows_router import router as rows_router
 from app.routers.rackspace.rack_router import router as racks_router
 from app.routers.objects.objects_router import router as objects_router
 from app.routers.objects.allocateObjects_router import router as allocate_router
+
 app = FastAPI()
 
 #describe version
-API_PREFIX = "/v0.3"
+API_PREFIX = "/v1"
 
 app.include_router(locations_router, prefix=API_PREFIX)
 app.include_router(rows_router, prefix=API_PREFIX)
 app.include_router(racks_router, prefix=API_PREFIX)
 app.include_router(objects_router, prefix=API_PREFIX)
 app.include_router(allocate_router, prefix=API_PREFIX)
+
+@app.get("/")
+def root():
+    return {"message" : "API running"}
